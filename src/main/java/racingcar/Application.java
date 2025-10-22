835429f6f5025de5e4f975a7878340fccda104c5
jungwook <mr4454@naver.com>
2025-10-22 14:06:00 +0900

    fix(getUserInput): fix a bug from Scanner
    Fix a bug from Scanner
    - close call early and cant get second input from Scanner before
    - now close call after getting second input and bug fix
    
    Breaks readLine and close is directly used instead of getUserInput

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,19 +7,13 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String> carNamesInOrder = validateCarNamesLength(parseCarNames(validateUserInput(getUserInput())));
+        List<String> carNamesInOrder = validateCarNamesLength(parseCarNames(validateUserInput(Console.readLine())));
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        long attemptNumber = Long.parseLong(validateAttemptNumberInput(validateUserInput(getUserInput())));
-
-
-    }
-
-    private static String getUserInput() {
-        String userInput = Console.readLine();
+        long attemptNumber = Long.parseLong(validateAttemptNumberInput(validateUserInput(Console.readLine())));
         Console.close();
 
-        return userInput;
+
     }
 
     private static String validateUserInput(String userInput) {
