829a15b2a6cf4f1eb063cdf910dfa41804433fa1
jungwook <mr4454@naver.com>
2025-10-22 10:29:50 +0900

    feat(guideAttemptNumberInput): implement guideAttemptNumberInput
    Implement guideAttemptNumberInput below main
    - get input from user
    - parseLong it and return

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,15 +3,25 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         List<String> cars = guideCarNamesInput();
 
+        long attemptNumber = guideAttemptNumberInput();
+
+
     }
+
+    private static long guideAttemptNumberInput() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String userInput = Console.readLine();
+        Console.close();
+
+
+        return Long.parseLong(userInput);
+    }
+
 
     private static List<String> guideCarNamesInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
