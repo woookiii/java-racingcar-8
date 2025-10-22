31ea94beeb05a4cbbb890ba94189a772ab22e7e7
jungwook <mr4454@naver.com>
2025-10-22 12:57:21 +0900

    refactor(xxxAttemptNumberInput): seperate validateAttemptNumberInput
    Seperate validateAttemptNumberInput from guideAttemptNumberInput
    - rename guideAttemptNumberInput to getAttemptNumberInput
    - print the guideline before getAttemptNumberInput running
    - parseLong from validation return string
    
    Breaks getAttemptNumberInput should be used instead of guideAttemptNumberInput

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,33 +8,34 @@ public class Application {
     public static void main(String[] args) {
         List<String> cars = guideCarNamesInput();
 
-        long attemptNumber = guideAttemptNumberInput();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        long attemptNumber = Long.parseLong(validateAttemptNumberInput(getAttemptNumberInput()));
 
 
     }
 
-    private static long guideAttemptNumberInput() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+    private static String getAttemptNumberInput() {
         String userInput = Console.readLine();
         Console.close();
 
-        validateAttemptNumberInput(userInput);
-
-        return Long.parseLong(userInput);
+        return userInput;
     }
 
-    private static void validateAttemptNumberInput(String userInput) {
+    private static String validateAttemptNumberInput(String userInput) {
         boolean isNumeric = userInput.chars().allMatch(Character::isDigit);
 
         if(!isNumeric || userInput.isEmpty()) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
+
+        return userInput;
     }
 
     private static List<String> guideCarNamesInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> cars = List.of(Console.readLine().split(","));
         Console.close();
+
         return cars;
     }
 }
